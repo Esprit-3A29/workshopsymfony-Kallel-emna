14e05cd440fa27497679cc0188bf00c3a8e90cd9
Kallel-emna <emna.kallel@esprit.tn>
2022-10-02 10:57:25 +0100

    correction erreur

--- src/Controller/ClubController.php
+++ src/Controller/ClubController.php
@@ -2,12 +2,15 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ClubController extends AbstractController
 {
+    /**
+     * @Route("/", methods={"GET"})
+     */
     #[Route('/club', name: 'app_club')]
     public function index(): Response
     {
